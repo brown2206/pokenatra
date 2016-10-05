0c49748e534cb19696980f6bc757fce844c8f3d4
brown2206 <brown.angelo.jr@gmail.com>
2016-10-05 15:25:16 -0400

    created new pokemon feature

--- app.rb
+++ app.rb
@@ -9,3 +9,23 @@ require_relative 'db/connection'
 require_relative 'models/pokemon'
 require_relative 'models/team'
 require_relative 'models/trainer'
+
+get '/pokemon' do
+  @pokemon = Pokemon.all
+  erb :"pokemon/index"
+end
+
+get '/pokemon/new' do
+  erb :"pokemon/new"
+end
+
+post '/pokemon' do
+  puts "*" * 40
+  @pokemon = Pokemon.create(params[:pokemon])
+  redirect "/pokemon/#{@pokemon.id}"
+end
+
+get '/pokemon/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"pokemon/show"
+end
